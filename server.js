80de8c1a649364c99398c491589a75c218124ab9
Josh Everett <josheverett93@gmail.com>
2020-04-17 18:08:32 -0500

    data returning from database file

--- server.js
+++ server.js
@@ -2,41 +2,62 @@
 // =============================================================
 const express = require("express");
 const path = require("path");
-const fs = require('fs');
+const fs = require("fs");
+const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// importing db.json
+const dbjson = require('./db/db.json');
 
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = process.env.PORT ||3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // holds user data
 let notes = [];
 
-// Basic route that sends the user first to the AJAX Page
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "public","index.html"));
-  });
+// Basic route that sends the user first to the Index Page
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public","notes.html"));
-  });
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
 
-app.post("/api/notes", function(req, res) {
-//   return res.json(reservations);
-  });
+app.get("/api/notes", function (req, res) {
+  // read db json file using fs and then:
+readFileAsync("./db/db.json", "utf8").then (data => {
+  const notesJSON = JSON.parse(data)
+  // parse when reading, stringify while writing
+    // if (err) throw err;
+    console.log(notesJSON)
+    res.json(notesJSON)
+  })
+});
 
-app.delete("/api/notes/:id", function(req, res) {
-//  return res.json(waitList);
-  });
+app.post("/api/notes", function (req, res) {
+  dbjson.push(req.body)
+  res.json(newNote)
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  dbjson.delete(req.body)
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
